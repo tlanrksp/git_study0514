ae2e0682f64fc437cdeb347579cca89ccabb34cd
tlanrksp <tlanrksp@naver.com>
2017-05-14 12:20:27 +0900

    first commit and push

---
+++ test1.js
@@ -0,0 +1,12 @@
+/**
+ * Created by XNOTE on 2017-05-14.
+ */
+
+var math = {
+    sum : function () {
+        for (var i = 0; i < arguments.length; i++) {
+            var obj = arguments[i];
+            
+        }
+    }
+}
